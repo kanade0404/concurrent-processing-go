a578bdc377b425f41fd07d02a7acdae2c01218f5
kanade <melty0404@gmail.com>
2022-04-23 15:33:48 +0900

    channel

--- luck_num/main.go
+++ luck_num/main.go
@@ -3,25 +3,22 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"sync"
 	"time"
 )
 
-func getLuckNum() {
+func getLuckNum(c chan<- int) {
 	fmt.Println("...")
 	rand.Seed(time.Now().Unix())
 	time.Sleep(time.Duration(rand.Intn(3000)) * time.Millisecond)
 	num := rand.Intn(10)
-	fmt.Printf("Today's your lucky number is %d!\n", num)
+	c <- num // 結果をチャネルに送信
 }
 
 func main() {
 	fmt.Println("what is today's lucky number?")
-	var wg sync.WaitGroup // 初期化時点でのカウンタは0
-	wg.Add(1)             // 内部カウントの値を+1する
-	go func() {
-		defer wg.Done() // ゴールーチンが終了した時にwgの内部カウントの値を-1するよう設定
-		getLuckNum()
-	}()
-	wg.Wait() // wgの内部カウンタが0になるまでメインゴールーチンをブロックして待つ
+	c := make(chan int) // チャネル作成
+	go getLuckNum(c)    // 引数にチャネルを渡す
+	num := <-c          // チャネルから結果を受信
+	fmt.Printf("Today's your lucky number is %d!\n", num)
+	close(c)
 }
