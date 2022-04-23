567cbc4303e030a1f54362f49ebfec327faea17a
kanade <melty0404@gmail.com>
2022-04-23 16:23:48 +0900

    concurrent-processing by channel

--- concurrent/main.go
+++ concurrent/main.go
@@ -2,17 +2,37 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"math/rand"
+	"time"
 )
 
+func getLuckyNum(c chan<- int) {
+	fmt.Println("...")
+	rand.Seed(time.Now().Unix())
+	time.Sleep(time.Duration(rand.Intn(3000)) * time.Millisecond)
+	num := rand.Intn(10)
+	c <- num // 結果をチャネルに送信
+}
+
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(3)
-	for i := 0; i < 3; i++ {
-		go func(i int) {
-			defer wg.Done()
-			fmt.Printf("count: %d", i)
-		}(i)
+	src := []int{1, 2, 3, 4, 5}
+	dst := []int{}
+	c := make(chan int)
+
+	// srcの要素毎にある何か処理をして、結果をdstにいれる
+	for _, s := range src {
+		go func(s int, c chan int) {
+			// 何か(重い)処理をする
+			time.Sleep(time.Second)
+			result := s * 2
+			c <- result
+		}(s, c)
 	}
-	wg.Wait()
+	for _ = range src {
+		num := <-c
+		dst = append(dst, num)
+	}
+
+	fmt.Println(dst)
+	close(c)
 }
